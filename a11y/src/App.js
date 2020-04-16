16d34b3aa290dedc29cabb2466aa148911c19127
bradserlis <bradserlis@yahoo.com>
2020-04-16 15:54:21 -0700

    code cleanup

--- a11y/src/App.js
+++ a11y/src/App.js
@@ -4,10 +4,16 @@ import './App.css';
 class App extends Component {
   render(){
     return (
-     <div>
+     <header>
+     <h1>'Ello World</h1>
+     <nav>
        <a href='#home'>Home</a>
        <img src='/img/is-an-image.jpg' alt=''/>
-     </div> 
+     </nav>
+     <form role='search'>
+       <input type='search' name='search' aria-label='search all content'/>
+     </form>
+     </header> 
   )
   }
 }
