f54e71a841097c32e3c4e875a7fabd3a8849c8cd
bradserlis <bradserlis@yahoo.com>
2020-04-15 17:41:40 -0700

    Add accessibility alt tag on image

---
+++ a11y/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+     <div>
+       <a href='#home'>Home</a>
+       <img src='/img/is-an-image.jpg' alt=''/>
+     </div> 
+  )
+}
+
+export default App;
+
